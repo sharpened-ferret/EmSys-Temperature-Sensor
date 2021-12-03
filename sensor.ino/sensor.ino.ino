db6b9763ee570b88e3b856b22aad799eb8c7aacf
Neil Woodhouse <neiltwoodhouse@gmail.com>
2021-12-03 15:15:38 +0000

    Added dotDevice.h

--- sensor.ino/sensor.ino.ino
+++ sensor.ino/sensor.ino.ino
@@ -1,21 +1,24 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include "dotDevice.h"
 
 #define ONE_WIRE_BUS 26
 
 OneWire oneWire(ONE_WIRE_BUS);
-
 DallasTemperature sensors(&oneWire);
+
+float temp_in_c;
 
 void setup() {
   Serial.begin(115200);
-  sensors.begin();
 }
 
 void loop() {
   sensors.requestTemperatures();
-  Serial.println(sensors.getTempCByIndex(0));
+  delay(1000);
+  temp_in_c = sensors.getTempCByIndex(0);
+  Serial.println(temp_in_c);
 
   
-  delay(1000);
+  
 }
