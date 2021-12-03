d9ff72b71314b49b4d2b2925a673e110f420d737
Neil Woodhouse <neiltwoodhouse@gmail.com>
2021-12-03 16:36:38 +0000

    WOORKING!

--- sensor.ino/sensor.ino.ino
+++ sensor.ino/sensor.ino.ino
@@ -7,18 +7,54 @@
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 
+const char* ssid = "NETGEAR35";
+const char* password = "magicalfinch482";
+const char* server = "ws://192.168.1.2:1234"; 
+const char* gid = "cag05ijt";
+
+dotDevice ddev(ssid, password, server);
+
 float temp_in_c;
+
+String jsonMsg;
+String jsonTemps;
+float total;
+float average;
+float current_temp;
+unsigned long start_time;
+unsigned long temp_time;
 
 void setup() {
   Serial.begin(115200);
+  ddev.connect();
 }
 
 void loop() {
-  sensors.requestTemperatures();
-  delay(1000);
-  temp_in_c = sensors.getTempCByIndex(0);
-  Serial.println(temp_in_c);
-
+  total = 0;
+  start_time = millis();
+  jsonMsg = "{\"device\": \"cag05ijt\", \"average\":";
+  jsonTemps = ", \"values\":["; 
   
+  
+  
+  for (int i = 0; i < 16; i++) {
+    sensors.requestTemperatures();
+    current_temp = sensors.getTempCByIndex(0);
+    temp_time = millis() - start_time;
+    total += current_temp;
+    jsonTemps = jsonTemps + "{\"timestamp\": " + temp_time + ", \"value\": " + current_temp + "}";
+
+    if (i < 15) {
+      jsonTemps += ", ";  
+    }
+    
+    Serial.println(current_temp);
+    delay(1500);
+  }
+  average = total / 16;
+  jsonMsg = jsonMsg + average + jsonTemps + "]}";
+  Serial.println("Block Complete!");
+  ddev.sendJSON(jsonMsg);
+  Serial.println(jsonMsg);  
   
 }
