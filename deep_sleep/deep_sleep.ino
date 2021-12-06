c9a9db630eb70c4698c81d2d26fa61d656424b1d
Neil Woodhouse <neiltwoodhouse@gmail.com>
2021-12-06 16:44:12 +0000

    Created basic deep_sleep version
    Takes 16 measurements, sends them, then deepsleeps for the remainder of the 30sec window

---
+++ deep_sleep/deep_sleep.ino
@@ -0,0 +1,60 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "dotDevice.h"
+
+#define ONE_WIRE_BUS 26
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+const char* ssid = "STINKnet2400";
+const char* password = "milkfast923";
+const char* server = "ws://ec2-52-15-138-171.us-east-2.compute.amazonaws.com:1234"; 
+const char* gid = "cag05ijt";
+
+dotDevice ddev(ssid, password, server);
+
+float temp_in_c;
+String jsonMsg = "{\"device\": \"cag05ijt\", \"average\":";
+String jsonTemps = ", \"values\":[";
+float total = 0;
+float average;
+float current_temp;
+unsigned long start_time;
+unsigned long temp_time;
+
+void setup() {
+  start_time = millis();
+  Serial.begin(115200);
+  ddev.connect();
+  delay(1000);
+
+  for (int i = 0; i < 15; i++) {
+    sensors.requestTemperatures();
+    current_temp = sensors.getTempCByIndex(0);
+    temp_time = millis() - start_time;
+    total += current_temp;
+    jsonTemps = jsonTemps + "{\"timestamp\": " + temp_time + ", \"value\": " + current_temp + "},";
+
+  }
+  sensors.requestTemperatures();
+  current_temp = sensors.getTempCByIndex(0);
+  temp_time = millis() - start_time;
+  total += current_temp;
+  jsonTemps = jsonTemps + "{\"timestamp\": " + temp_time + ", \"value\": " + current_temp + "}";
+ 
+  
+  average = total / 16;
+  jsonMsg = jsonMsg + average + jsonTemps + "]}";
+  Serial.println("Sending Data");
+  ddev.sendJSON(jsonMsg);
+
+  Serial.println("Going to sleep");
+  Serial.println(millis());
+  unsigned long sleep_time = (30000 - (millis()-start_time))*1000;
+  Serial.println(sleep_time);
+  esp_sleep_enable_timer_wakeup(sleep_time);
+  esp_deep_sleep_start();
+}
+
+void loop() {}
