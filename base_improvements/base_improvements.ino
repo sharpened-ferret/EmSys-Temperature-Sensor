0403eb40c9cdfed589b5d874d8b4bc8b3d12e107
Neil Woodhouse <clonesupreme@gmail.com>
2021-12-06 15:03:22 +0000

    Added a general improved json version

---
+++ base_improvements/base_improvements.ino
@@ -0,0 +1,58 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "dotDevice.h"
+
+#define ONE_WIRE_BUS 26
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+const char* ssid = "NETGEAR35";
+const char* password = "magicalfinch482";
+const char* server = "ws://192.168.1.2:1234"; 
+const char* gid = "cag05ijt";
+
+dotDevice ddev(ssid, password, server);
+
+float temp_in_c;
+
+String jsonMsg;
+String jsonTemps;
+float total;
+float average;
+float current_temp;
+unsigned long start_time;
+unsigned long temp_time;
+
+void setup() {
+  ddev.connect();
+}
+
+
+//Removed serial printouts and eliminated if statement
+void loop() {
+  total = 0;
+  start_time = millis();
+  jsonMsg = "{\"device\": \"cag05ijt\", \"average\":";
+  jsonTemps = ", \"values\":["; 
+  
+  
+  for (int i = 0; i < 15; i++) {
+    sensors.requestTemperatures();
+    current_temp = sensors.getTempCByIndex(0);
+    temp_time = millis() - start_time;
+    total += current_temp;
+    jsonTemps = jsonTemps + "{\"timestamp\": " + temp_time + ", \"value\": " + current_temp + "},";
+    delay(1244);
+  }
+  sensors.requestTemperatures();
+  current_temp = sensors.getTempCByIndex(0);
+  temp_time = millis() - start_time;
+  total += current_temp;
+  jsonTemps = jsonTemps + "{\"timestamp\": " + temp_time + ", \"value\": " + current_temp + "}";
+
+  
+  average = total / 16;
+  jsonMsg = jsonMsg + average + jsonTemps + "]}";
+  ddev.sendJSON(jsonMsg);
+}
